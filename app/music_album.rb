cd065aea1bf3aea4dad1875e85e3fe8d3318fce0
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-18 00:07:32 +0300

    created MusicAlbum class

---
+++ app/music_album.rb
@@ -0,0 +1,14 @@
+require_relative '../item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify, :name 
+
+  def initialize(on_spotify, id:nil publish_date)
+    super(id publish_date)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?()
+    true if super() && @on_spotify
+  end
+end

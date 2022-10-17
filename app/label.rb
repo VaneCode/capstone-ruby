cf154ba8cac888e4c33c6021da91e3fc991f3c22
vanessaBonito <vanebonito@hotmail.com>
2022-10-17 16:56:48 -0500

    Add class Label with its methods

--- app/label.rb
+++ app/label.rb
@@ -1,1 +1,28 @@
+require 'securerandom'
+require 'set'
+require_relative 'item'
 
+class Label
+  attr_accessor :name
+  attr_reader :id, :items
+
+  def initialize(id:, name:)
+    @id = id || SecureRandom.random_number(1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_label(self)
+  end
+
+  def list_labels(books)
+    labels = Set.new
+    books.each { |book| labels << book.label }
+    puts "You have #{labels.size} labels:"
+    labels.each_with_index do |label, index|
+      puts "#{index + 1} Label: #{label.name}"
+    end
+  end
+end

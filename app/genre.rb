3ae3242b8064e5ffbb4f5e70fa656f53a5d39142
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-18 00:21:08 +0300

    Implement add_item method in genre class

--- app/genre.rb
+++ app/genre.rb
@@ -3,9 +3,14 @@ require_relative './musicalbum'
 class Genre
   attr_accessor :items, :name, :id
 
- def initialize(name)
+  def initialize(name)
     @id = Random.rand(1...1000)
     @name = name
     @items = []
+  end
+  
+  def add_item(item)
+    @items << item
+    item.genre = self
   end
 end

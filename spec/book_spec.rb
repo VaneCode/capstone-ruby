3c658a2db25bfbb73f91193bf8a83db83a27f0d5
vanessaBonito <vanebonito@hotmail.com>
2022-10-17 14:02:23 -0500

    Create folders and files for all classes

---
+++ spec/book_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../app/book'
+
+describe Item do
+  before :each do
+    @item = Item.new(publish_date: '1990-01-20', archived: false)
+  end
+
+  it 'Should be an object instance of Item' do
+    expect(@item).to be_an_instance_of Item
+  end
+
+  it 'Method move_to_archive should change atribute archived to true' do
+    @item.move_to_archive
+    expect(@item.archived).to eql true
+  end
+end

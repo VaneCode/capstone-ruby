72b85aef30ce28185d2527e937a283f758578f5b
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-18 00:16:58 +0300

    Add constructor with genre visible properties on diagram

--- app/genre.rb
+++ app/genre.rb
@@ -1,7 +1,11 @@
 require_relative './musicalbum'
 
 class Genre
-  attr_accessor :items, :name
+  attr_accessor :items, :name, :id
 
- 
+ def initialize(name)
+    @id = Random.rand(1...1000)
+    @name = name
+    @items = []
+  end
 end

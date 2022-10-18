df1ebae5954f9d2d1c75422a466fd60ec385a03e
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-19 00:05:16 +0300

    Add test to check if the genre has name propert

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,8 +1,12 @@
-require_relative '../item'
-require_relative '../genre'
+require_relative '../app/genre'
+require_relative '../app/genre'
 
 describe Genre do
  before :each do
     @genre = Genre.new('Fantasy')
+  end
+
+  it 'has a name' do
+    expect(@genre.name).to eq('Fantasy')
   end
 end

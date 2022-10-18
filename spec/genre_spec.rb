fe9417a0478ff6974a56ae14aeb08cda7726939c
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-19 00:20:17 +0300

    Add test for checking if a genre object has been created

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -6,6 +6,10 @@ describe Genre do
     @genre = Genre.new('Fantasy')
   end
 
+  it 'takes in one parameter and returns a Genre object' do
+      expect(@genres).to be_an_instance_of(Genre)
+    end
+
   it 'has a name' do
     expect(@genre.name).to eq('Fantasy')
   end

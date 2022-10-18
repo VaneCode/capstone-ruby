233cbc08d3d71876facf7284e714b7104fd85be0
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-19 00:31:21 +0300

    Modify genre test file

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -7,16 +7,11 @@ describe Genre do
   end
 
   it 'takes in one parameter and returns a Genre object' do
-      expect(@genres).to be_an_instance_of(Genre)
+      expect(@genre).to be_an_instance_of(Genre)
     end
 
   it 'has a name' do
     expect(@genre.name).to eq('Fantasy')
-  end
-
-  it 'is able to add items' do
-    @genre.add_item(Item.new('2000-01-01'))
-    expect(@genre.items.length).to eq(1)
   end
 
   it 'contains added genre items' do

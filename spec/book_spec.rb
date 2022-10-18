3e26cfb82e9a35b2eb8a996a69e692bb87c4fba6
vanessaBonito <vanebonito@hotmail.com>
2022-10-17 19:02:40 -0500

    Add tests for class Book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,16 +1,27 @@
 require_relative '../app/book'
+require_relative '../app/label'
 
-describe Item do
+describe Book do
   before :each do
-    @item = Item.new(publish_date: '1990-01-20', archived: false)
+    @book = Book.new(publisher: 'Pinguin Readers', cover_state: 'bad', publish_date: '2018-01-20', archived: false)
+    @label = Label.new(id: nil, name: 'Cookie')
+    @label.add_item(@book)
   end
 
-  it 'Should be an object instance of Item' do
-    expect(@item).to be_an_instance_of Item
+  it 'Should be an object instance of Book' do
+    expect(@book).to be_an_instance_of Book
   end
 
-  it 'Method move_to_archive should change atribute archived to true' do
-    @item.move_to_archive
-    expect(@item.archived).to eql true
+  it 'Method getter for publisher should return Pinguin Readers' do
+    expect(@book.publisher).to eql 'Pinguin Readers'
+  end
+
+  it 'Method getter for cover_state should return bad' do
+    expect(@book.cover_state).to eql 'bad'
+  end
+
+  it 'Method can_use_services? should return true because cover_state is equal to bad' do
+    @book.move_to_archive
+    expect(@book.archived).to eql true
   end
 end

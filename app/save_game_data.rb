29bb1e8fc0c594fa62b40cdbc110f081087a9822
ijeoma <ijeomaodinko>
2022-10-19 09:43:44 -0400

    update on savegame method

--- app/save_game_data.rb
+++ app/save_game_data.rb
@@ -4,11 +4,11 @@ module SaveGame
     def self.save_game(games)
         game_json = []
         games.each do |game|
-          save_game  = { name: games.name, publish_date: games.publish_date
-           mutiplayer: games.multiplayer  last_played_at: games.last_played_at }
+          save_game  = { name: game.name, publish_date: game.publish_date,
+           mutiplayer: game.multiplayer,  last_played_at: game.last_played_at }
         game_json << save_game
         end
-        File.write('./data/games.json', JSON.generate(game_json))
+        File.write('./app/games.json', JSON.generate(game_json))
     end
 
 
@@ -30,10 +30,10 @@ module SaveGame
     File.write('./data/author.json', JSON.generate(author_save))
  end
  def read_game(games)
-    return books unless File.exist?('./app/')
-   gamearr = JSON.parse(File.read('./app/author.json'))
+    return books unless File.exist?('./app/game.json')
+   gamearr = JSON.parse(File.read('./app/game.json'))
    gamearr.each do |game|
-    game_json = Game.new
+    game_json = Game.new(game['name'], game ['publish_date'], game['multiplayer'], game['last_played_at'])
     games << game_json
    end
 end
@@ -45,8 +45,7 @@ end
         author.last_name = author['last_name']
         author.items = author ['games'].map do |game|
         Game.new(game['name'], game['last_played_at'], game ['publish_date'])
-        end 
-    read_json(author, 'authors.json')
+     
  end
 
       

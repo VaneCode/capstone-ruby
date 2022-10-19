15b3417e6b59a47a74e2c0b136f1825f2ec7931a
ijeoma <ijeomaodinko>
2022-10-19 09:10:57 -0400

    create the method to preserve data in games.json file

--- app/save_game_data.rb
+++ app/save_game_data.rb
@@ -7,7 +7,10 @@ module SaveGame
           save_game << { 'name:' => games.name, 'publish_date:' => games.publish_date
           'mutiplayer:' => games.multiplayer  'last_played_at: ' => games.last_played_at }
         end
-    
+        File.write('./data/games.json', JSON.generate(save_game))
+    end
+
+
      
 
 end

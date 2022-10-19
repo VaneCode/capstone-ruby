dd02da2f7e39823c83317a513ac0e1fef75e9628
ijeoma <ijeomaodinko>
2022-10-19 01:02:20 -0400

    Add a method to create game

--- app/game.rb
+++ app/game.rb
@@ -11,7 +11,20 @@ class Game < Item
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
+  def self.add_game(games)
+    print 'Please enter the name of the game'
+    name = gets.chomp
+    print 'Enter the published date:'
+    publish_date = gets.chomp
+    print 'Enter if is multiplayed'
+    multiplayer = gets.chomp
+    print 'Enter the when last played'
+    last_played_at = gets.chomp
+    game = Game.new(name, publish_date, multiplayer, last_played_at)
+    games << game
+  end
 
+  
   
 end
 game = Game.new(name: 'Solatire', publish_date: '2022/10/9', multiplayer: true, last_played_at: '2018/10/10')

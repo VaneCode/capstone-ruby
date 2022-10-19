77d86b6bc7f2a2fb6f52cc474edfb74c401fc010
ijeoma <ijeomaodinko>
2022-10-19 09:37:26 -0400

    update on savegame method

--- app/save_game_data.rb
+++ app/save_game_data.rb
@@ -27,16 +27,24 @@ module SaveGame
          end
        }
     end 
-    write_json(author, 'authors.json')
+    File.write('./data/author.json', JSON.generate(author_save))
  end
- 
+ def read_game(games)
+    return books unless File.exist?('./app/')
+   gamearr = JSON.parse(File.read('./app/author.json'))
+   gamearr.each do |game|
+    game_json = Game.new
+    games << game_json
+   end
+end
+
  def read_author 
     file = read_json(authors.json)
     file.each do |author|
         author = Author.new(author['first_name'])
         author.last_name = author['last_name']
         author.items = author ['games'].map do |game|
-            Game.new(game['name'], game['last_played_at'], game ['publish_date'])
+        Game.new(game['name'], game['last_played_at'], game ['publish_date'])
         end 
     read_json(author, 'authors.json')
  end
